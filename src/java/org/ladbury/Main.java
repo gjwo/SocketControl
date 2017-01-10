b85a843d36526a4ec51972d8412cef16ad225225
gjwo <grahamjwood@gmail.com>
2017-01-10 13:41:58 +0000

    attempt to resolve problems with finding Main class (not Working)

--- src/java/org/ladbury/Main.java
+++ src/java/org/ladbury/Main.java
@@ -1,22 +1,26 @@
 package org.ladbury;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
+
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void main(String[] args) {
-	SocketControl s = new SocketControl();
-    System.out.print("To clear the socket programming, press the green button");
-    System.out.print("To clear the socket programming, press the green button");
-    System.out.print("for 5 seconds or more until the red light flashes slowly");
-    System.out.print("The socket is now in its learning mode and listening for");
-    System.out.print("a control code to be sent. ");
-	s.switchSocket(SocketControl.SocketCode.SOCKET1, SocketControl.SocketState.ON);
+    public static void main(String[] args)
+    {
+        SocketControl s = new SocketControl();
+        System.out.print("To clear the socket programming, press the green button");
+        System.out.print("To clear the socket programming, press the green button");
+        System.out.print("for 5 seconds or more until the red light flashes slowly");
+        System.out.print("The socket is now in its learning mode and listening for");
+        System.out.print("a control code to be sent. ");
+        s.switchSocket(SocketControl.SocketCode.SOCKET1, SocketControl.SocketState.ON);
         try
         {
             TimeUnit.SECONDS.sleep(3);
         } catch (InterruptedException e)
         {
+            Ignore;
         }
         s.switchSocket(SocketControl.SocketCode.SOCKET1, SocketControl.SocketState.ON);
         try
